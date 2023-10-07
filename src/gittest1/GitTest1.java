d49820f845f36fe22938941e2b4d3edf172080e4
Hasindu Gunathilaka <hasindumihiran18@gmail.com>
2023-10-07 17:05:46 +0530

    Test Git1

--- src/gittest1/GitTest1.java
+++ src/gittest1/GitTest1.java
@@ -10,11 +10,11 @@ package gittest1;
  */
 public class GitTest1 {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("Hello world");
     }
     
 }
